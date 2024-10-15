254237f9b7e5419a36bf7779073884359fc19480
Gaston <pallasgaston@gmail.com>
2024-10-15 14:36:47 +0200

    Clean Code

--- Back/Back/src/main/java/com/example/back/Process.java
+++ Back/Back/src/main/java/com/example/back/Process.java
@@ -34,9 +34,6 @@ public class Process {
     public static final LocalDateTime DATE_20_MINUTES_AGO = LocalDateTime.now().minusMinutes(2000000000);
 
     public void startProcess(){
-        System.out.println("test");
-
-
 
         // Récupérer la liste des clients
         List<Client> listeClient = clientRepository.findAll();
@@ -47,39 +44,8 @@ public class Process {
             // brunoClass(client);
             String token = "token";
 
-            // Appel à l'API pour récupérer tout les messages -> simulation
-            // String instagramMessagesUrl = "https://graph.instagram.com/me/messages?access_token=" + accessToken;
-            String url = "https://79938d12-de45-49b7-95b9-4d5327d3f5ed.mock.pstmn.io/instagram/messages?token=" + token;
+            ArrayList<Data> dataList = callAPIInstagram(token);
 
-            // Créer le client HTTP
-            HttpClient httpClient = HttpClient.newHttpClient();
-
-            // Créer la requête GET
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .GET()
-                    .build();
-
-            // Envoyer la requête et recevoir la réponse
-            HttpResponse<String> response = null;
-            try {
-                response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            // Afficher la réponse
-            System.out.println(response.body());
-            // Convertir la réponse JSON en objets Java (POJOs)
-
-            ObjectMapper mapper = new ObjectMapper();
-            MessageResponse messageResponse;
-            try {
-                messageResponse = mapper.readValue(response.body(), MessageResponse.class);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            ArrayList<Data> dataList = messageResponse.getData();
             dataList.forEach((data) -> {
                 // Pour chaque message
                 // Filtre sur la date, si c'est abonnée ou pas
@@ -90,18 +56,55 @@ public class Process {
                     if(localDate.isAfter( DATE_20_MINUTES_AGO.toLocalDate()) && !data.getRecipient().isFollower()){
 
                         // Classer le message grâce à l'IA
+                        callAPIPerspective(data);
 
-                        // SI catégorie A = API instagram pour supprimer, voir bloquer, voir masquer etc.... -> simulation
-
-                        // Historiser les messages
                     }
                 } catch (DateTimeParseException e) {
                     System.err.println("Erreur lors de la conversion: " + e.getMessage());
                 }
             });
-
-
-
         }));
     }
+
+    private ArrayList<Data> callAPIInstagram(String token) {
+        // Appel à l'API pour récupérer tout les messages -> simulation
+        String url = "https://79938d12-de45-49b7-95b9-4d5327d3f5ed.mock.pstmn.io/instagram/messages?token=" + token;
+
+        // Créer le client HTTP
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        // Créer la requête GET
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        // Envoyer la requête et recevoir la réponse
+        HttpResponse<String> response = null;
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.body());
+        // Convertir la réponse JSON en objets Java (POJOs)
+
+        ObjectMapper mapper = new ObjectMapper();
+        MessageResponse messageResponse;
+        try {
+            messageResponse = mapper.readValue(response.body(), MessageResponse.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return messageResponse.getData();
+    }
+
+    private void callAPIPerspective(Data data) {
+        // SI catégorie A = API instagram pour supprimer, voir bloquer, voir masquer etc.... -> simulation
+
+        System.out.println(data.getMessage());
+        // Historiser les messages
+    }
+
 }
