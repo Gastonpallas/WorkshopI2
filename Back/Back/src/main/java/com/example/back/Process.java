dfa5d53fb3000166f8f643734570a692966a9180
Gaston <pallasgaston@gmail.com>
2024-10-15 11:15:21 +0200

    Simulation API Instagram

--- Back/Back/src/main/java/com/example/back/Process.java
+++ Back/Back/src/main/java/com/example/back/Process.java
@@ -5,7 +5,12 @@ import com.example.back.Client.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.http.HttpClient;
 import java.util.List;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
 public class Process {
@@ -27,9 +32,31 @@ public class Process {
         listeClient.forEach((client -> {
             // récupérer le token
             // brunoClass(client);
+            String token = "token";
 
             // Appel à l'API pour récupérer tout les messages -> simulation
             // String instagramMessagesUrl = "https://graph.instagram.com/me/messages?access_token=" + accessToken;
+            String url = "https://a34085de-49ac-4e44-905d-f7907e1849d1.mock.pstmn.io/instagram/messages?token=" + token;
+
+            // Créer le client HTTP
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            // Créer la requête GET
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            // Envoyer la requête et recevoir la réponse
+            HttpResponse<String> response = null;
+            try {
+                response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Afficher la réponse
+            System.out.println(response.body());
 
             // Filtre sur la date, si c'est abonnée ou pas
 
